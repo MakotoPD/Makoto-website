99f4a150814b744ef105c79e7a2fb0f143848572
MakotoPD <48807218+MakotoPD@users.noreply.github.com>
2025-09-17 12:38:21 +0200

    remove api link

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -82,7 +82,7 @@ export default defineNuxtConfig({
 	runtimeConfig: {
 
 		public: {
-			apiUrl: process.env.NUXT_PUBLIC_API_URL || 'https://api.makoto.com.pl'
+			apiUrl: process.env.NUXT_PUBLIC_API_URL
 		}
 	},
 
