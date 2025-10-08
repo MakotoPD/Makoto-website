fa3553869f5ab7864ce01843677184ab7495d921
MakotoPD <48807218+MakotoPD@users.noreply.github.com>
2025-10-08 22:59:29 +0200

    add some panels

---
+++ app/pages/admin.vue
@@ -0,0 +1,78 @@
+<template>
+	<div class="pt-24">
+		<div class="max-w-2xl mx-auto flex flex-col items-center">
+			<h1 class="text-6xl">
+				Not today ;)
+			</h1>
+
+			<div class="mt-24">
+				<NuxtLink :to="localePath('/')" class="outer-cont flex">
+				<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><!-- Icon from Solar by 480 Design - https://creativecommons.org/licenses/by/4.0/ --><path fill="currentColor" d="M17.75 19a.75.75 0 0 1-1.32.488l-6-7a.75.75 0 0 1 0-.976l6-7A.75.75 0 0 1 17.75 5z" opacity=".5"/><path fill="currentColor" fill-rule="evenodd" d="M13.488 19.57a.75.75 0 0 0 .081-1.058L7.988 12l5.581-6.512a.75.75 0 1 0-1.138-.976l-6 7a.75.75 0 0 0 0 .976l6 7a.75.75 0 0 0 1.057.082" clip-rule="evenodd"/></svg>
+				Go back home
+				</NuxtLink>
+			</div>
+		</div>
+
+
+
+	</div>
+</template>
+<script setup>
+const localePath = useLocalePath()
+</script>
+
+
+<style scoped>
+  .flex {
+    display: flex;
+    align-items: center;
+    gap: 5px;
+  }
+  .outer-cont {
+    padding: 12px 20px;
+    border: none;
+    font-size: 1rem;
+    cursor: pointer;
+    position: relative;
+    background: linear-gradient(90deg, #5bfcc4, #f593e4, #71a4f0);
+    border-radius: 12px;
+    color: #fff;
+    transition: all 0.3s ease;
+    box-shadow:
+      inset 0px 0px 5px #ffffffa9,
+      inset 0px 35px 30px #000,
+      0px 5px 10px #000000cc;
+    text-shadow: 1px 1px 1px #000;
+  }
+  .outer-cont::before {
+    content: "";
+    position: absolute;
+    inset: 0;
+    margin: auto;
+    border-radius: 12px;
+    filter: blur(0);
+    z-index: -1;
+    box-shadow: none;
+    background: conic-gradient(
+      #00000000 80deg,
+      #40baf7,
+      #f34ad7,
+      #5bfcc4,
+      #00000000 280deg
+    );
+    transition: all 0.3s ease;
+  }
+  .outer-cont:hover::before {
+    filter: blur(15px);
+  }
+  .outer-cont:active::before {
+    filter: blur(5px);
+    transform: translateY(1px);
+  }
+  .outer-cont:active {
+    box-shadow:
+      inset 0px 0px 5px #ffffffa9,
+      inset 0px 35px 30px #000;
+    margin-top: 3px;
+  }
+</style>
---
+++ app/pages/panel/index.vue
@@ -0,0 +1,10 @@
+<template>
+	<div class="pt-24">
+		<div class="max-w-2xl mx-auto flex flex-col items-center">
+			<h1 class="text-6xl">
+				Admin panel
+			</h1>
+		</div>
+
+	</div>
+</template>
